ebc78fe03774472954661b95923ad812a902887a
Shradha Limbu <shradhatumbahangphe@gmail.com>
2026-08-24 22:55:50 +0545

    Update exam planning

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userroute.js";
 import classRoutes from "./routes/classRoute.js";
 import sectionRoutes from "./routes/sectionRoute.js";
 import examRoute from "./routes/examRoute.js";
+import questionPaperRoute from "./routes/questionPaperRoute.js";
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/classes", classRoutes);
 app.use("/api/sections", sectionRoutes);
 app.use("/api/exams", examRoute);
+app.use("/api/question-papers", questionPaperRoute);
 
 
 export default app;
